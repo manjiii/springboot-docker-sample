dc8220447552ee86ab933e70dbe4624e701e44e7
manjiii <manjix3@gmail.com>
2020-03-14 19:19:54 +0000

    add logger

--- app/src/main/java/com/example/demo/controllers/IndexController.java
+++ app/src/main/java/com/example/demo/controllers/IndexController.java
@@ -4,13 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 public class IndexController {
 
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+    
     @RequestMapping({ "/", "/index" })
     public ModelAndView get(ModelAndView mav) {
         mav.setViewName("index");
-        System.out.println("aaa");
+        logger.debug("logger debug");
+        logger.info("logger info");
+        logger.warn("logger warn");
+        logger.error("logger error");
         return mav;
     }
 }
